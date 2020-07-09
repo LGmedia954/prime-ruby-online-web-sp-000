80625cae682578958942680f2c073490eba4bb97
Leslie G <lesliegonzalez@me.com>
2020-07-09 00:57:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,10 @@
 # Add  code here!
 
 def prime?()
- (int..(int - 1)).each do |n|
-    return false if int % n == 0
+ n = 2
+  while n < num
+    return false if num % n == 0
+    n += 1
   end
   true
 end
